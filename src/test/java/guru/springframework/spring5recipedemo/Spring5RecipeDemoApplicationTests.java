e3a490956d6340ea6a0c867ed277cf71f4350b27
aricson <aricsondr@gmail.com>
2020-03-09 16:10:44 -0100

    Initialize Recipe Spring Project

---
+++ src/test/java/guru/springframework/spring5recipedemo/Spring5RecipeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
